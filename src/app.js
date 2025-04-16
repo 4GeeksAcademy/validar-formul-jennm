43207dd67f0472f9408ce0612c235819a5b807ae
Jennifer <jenncalde1588@gmail.com>
2025-04-16 16:09:13 +0000

    validarFormular

--- src/app.js
+++ src/app.js
@@ -6,6 +6,48 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  const alertBox = document.querySelector("#alert-message"); 
+  const cardInput = document.getElementById("cardNumber");
+  const cvcInput = document.getElementById("cvc");
+  const amountInput = document.getElementById("amount");
+  const firstNameInput = document.getElementById("firstName");
+  const lastNameInput = document.getElementById("lastName");
+  const cityInput = document.getElementById("city");
+  const stateSelect = document.getElementById("state");
+  const postalCodeInput = document.getElementById("postalCode");
+  const messageInput = document.getElementById("comment");
+  const sendBtn = document.querySelector(".btn-primary");
+
+  sendBtn.addEventListener("click", function (event) {
+    event.preventDefault();
+
+    const inputs = [  
+      cardInput, cvcInput, amountInput, firstNameInput,
+      lastNameInput, cityInput, stateSelect, postalCodeInput, messageInput
+    ];
+    console.log(inputs)
+    let hasError = false;
+    inputs.forEach(input => input.style.backgroundColor = "");
+
+    inputs.forEach(input => {
+      if (input.value.trim() === "") {
+        input.style.backgroundColor = "#f8d7da";
+        hasError = true;
+      }
+    });
+
+    if (stateSelect.value === "Pick a state") {
+      stateSelect.style.backgroundColor = "#f8d7da";
+      hasError = true;
+    } else {
+      stateSelect.style.backgroundColor = "";
+    }
+
+    if (hasError) {
+      alertBox.style.display = "block";
+    } else {
+      alertBox.style.display = "none";
+      alert("Formulario enviado correctamente ✅");
+    }
+  });
 };
